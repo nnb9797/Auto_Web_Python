ddffd3c805915794f34dfb8c9fd34518b0d118b3
nnb9797 <108178648+nnb9797@users.noreply.github.com>
2023-09-29 19:34:07 +0700

    Merge remote-tracking branch 'origin/master'

--- HW_3/conftest.py
+++ HW_3/conftest.py
@@ -22,31 +22,3 @@ def browser():
     yield driver
     driver.quit()
 
-@pytest.fixture()
-def x_selector1():
-    return """//*[@id="login"]/div[1]/label/input"""
-
-
-@pytest.fixture()
-def x_selector2():
-    return """//*[@id="login"]/div[2]/label/input"""
-
-
-@pytest.fixture()
-def x_selector3():
-    return """//*[@id="app"]/main/div/div/div[2]/h2"""
-
-
-@pytest.fixture()
-def btn_selector():
-    return "button"
-
-
-@pytest.fixture()
-def result():
-    return "401"
-
-
-@pytest.fixture()
-def hello_user():
-    return """//*[@id="app"]/main/nav/ul/li[3]/a"""
